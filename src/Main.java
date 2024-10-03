646126bee022dd410099070733741c73578eb19b
markb <mark_batmanov@mail.ru>
2024-10-03 06:50:29 +0300

    Objects and classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author author = new Author("Йон", "Несбё");
+        System.out.println(author.getFirstName());
+        System.out.println(author.getLastName());
+
+        Book book = new Book("Охотники за головами", 2008, author);
+        System.out.println("Название книги: " + book.getName());
+        System.out.println("Год публикации: " + book.getPublicationYear());
+
+        book.setPublicationYear(2009);
+
+        System.out.println("Год публикации: " + book.getPublicationYear());
+
     }
 }
